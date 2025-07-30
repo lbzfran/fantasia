56058e9d540e6ae9ede91bc61d78fb551a6a1bfe
Liam Bagabag <lbscout1@gmail.com>
2025-07-30 13:55:41 -0700

    added more logic

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 #include <stddef.h>
 #include <uchar.h>
 
+
+#include <stdio.h>
+
 typedef char        byte;
 typedef char        char8;
 typedef char16_t    char16;
@@ -25,17 +28,16 @@ typedef size_t      usize;
 typedef ptrdiff_t   ssize;
 typedef uintptr_t   uintptr;
 
-#define assert(c)   while (!(c)) __builtin_unreachable()
+#define assert(c)               while (!(c)) __builtin_unreachable()
 
-#define sizeof(x)       (ssize)sizeof(x)
-#define alignof(x)      (_Alignof(x))
-#define countof(a)      (sizeof(a) / sizeof(*(a)))
-#define lengthof(s)     (countof(s) - 1)
-#define signof(x)       ((x) > 0) ? 1 : (((x) < 0) ? -1 : 0)
+#define sizeof(x)               (ssize)sizeof(x)
+#define alignof(x)              (_Alignof(x))
+#define countof(a)              (sizeof(a) / sizeof(*(a)))
+#define lengthof(s)             (countof(s) - 1)
+#define signof(x)               ((x) > 0) ? 1 : (((x) < 0) ? -1 : 0)
 
-#define coalesce(a, b)  ((a) ? (a) : (b))
-
-#define init_if_null(a, x)    ((a) = coalesce((a), (x)))
+#define coalesce(a, b)          ((a) ? (a) : (b))
+#define init_if_null(a, x)      ((a) = coalesce((a), (x)))
 
 #define not     !
 #define is      ==
@@ -64,54 +66,121 @@ typedef struct Mob {
     bool32 initialized;
 } Mob;
 
+void Vector2Print(Vector2 v) {
+    printf("v:(%f, %f)\n", v.x, v.y);
+}
+
+
 void MobUpdate(Mob *mob, Vector2 direction, float32 dt) {
     if (not mob->initialized) {
-        init_if_null(mob->scale.x, 1.0f);
-        init_if_null(mob->scale.y, 1.0f);
+        init_if_null(mob->position.x, (float)GetScreenWidth()/2);
+        init_if_null(mob->position.y, (float)GetScreenHeight()/2);
+
+        init_if_null(mob->scale.x, 100.0f);
+        init_if_null(mob->scale.y, 100.0f);
 
         init_if_null(mob->direction.x, 1.0f);
         init_if_null(mob->direction.y, 1.0f);
 
-        init_if_null(mob->speed, 1200.0f);
-        init_if_null(mob->friction, 1.0f);
+        init_if_null(mob->speed, 80.0f);
+        init_if_null(mob->friction, 0.1f);
 
         mob->initialized = true;
     }
 
     Vector2 velocity = Vector2Subtract(mob->position, mob->last_position);
-    Vector2 acceleration = (Vector2){ 0.0f, 0.0f };
+    Vector2 acceleration = Vector2Zero();
 
     if (mob->position.x < 0.0f) {
         mob->position.x = 0.0f;
         mob->last_position.x = mob->position.x + velocity.x;
     }
-    // else if ...
+    else if (mob->position.x > GetScreenWidth() - mob->scale.x) {
+        mob->position.x = GetScreenWidth() - mob->scale.x;
+        mob->last_position.x = mob->position.x + velocity.x;
+    }
     if (mob->position.y < 0.0f) {
         mob->position.y = 0.0f;
         mob->last_position.y = mob->position.y + velocity.y;
     }
-    // else if ...
+    else if (mob->position.y > GetScreenHeight() - mob->scale.y) {
+        mob->position.y = GetScreenHeight() - mob->scale.y;
+        mob->last_position.y = mob->position.y + velocity.y;
+    }
 
     mob->direction.x = coalesce(direction.x, mob->direction.x);
     mob->direction.y = coalesce(direction.y, mob->direction.y);
     direction = Vector2Normalize(direction);
+
+    if (Vector2Length(direction) > 0) {
+        acceleration = Vector2Add(acceleration, Vector2Scale(direction, 4000.0f * mob->speed));
+    }
+    else if (Vector2Length(velocity) > 0) {
+        acceleration = Vector2Subtract(acceleration, Vector2Scale(velocity, mob->friction));
+    }
+
+    mob->last_position = mob->position;
+    // NOTE: mob->position += (velocity + acceleration * dt) * dt;
+    Vector2 new_position;
+    new_position = Vector2Scale(acceleration, dt);
+    new_position = Vector2Add(velocity, new_position);
+    new_position = Vector2Scale(new_position, dt);
+
+    mob->position = Vector2Add(mob->position, new_position);
+
+    Vector2Print(velocity);
+    Vector2Print(acceleration);
+}
+
+void MobRender(Mob *mob) {
+    DrawRectangleV(mob->position, mob->scale, BLACK);
 }
 
 typedef struct World {
     Mob mobs[255];
+    uint8 mob_count;
 } World;
+World world = {};
+
 
 int main(void) {
     InitWindow(800, 600, "Fantasia");
     bool running = true;
+    world.mobs[world.mob_count++] = (Mob){ 0 };
 
     while (running) {
+        float dt = GetFrameTime();
         if (WindowShouldClose() || IsKeyPressed(KEY_Q)) {
             running = false;
         }
 
+        Vector2 player_direction = Vector2Zero();
+        if (IsKeyDown(KEY_W)) {
+            player_direction.y -= 1;
+        }
+        if (IsKeyDown(KEY_S)) {
+            player_direction.y += 1;
+        }
+        if (IsKeyDown(KEY_A)) {
+            player_direction.x -= 1;
+        }
+        if (IsKeyDown(KEY_D)) {
+            player_direction.x += 1;
+        }
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
+
+            for (uint8 i = 0; i < world.mob_count; i++) {
+                if (i == 0) {
+                    MobUpdate(&world.mobs[i], player_direction, dt);
+                }
+                else {
+                    MobUpdate(&world.mobs[i], Vector2One(), dt);
+                }
+                MobRender(&world.mobs[i]);
+            }
+
         EndDrawing();
     }
 
